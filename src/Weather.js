7520274c37a7e7e8b8f48a600c6027edd14b561b
Matin Torkian <matin.torkian.92@gmail.com>
2023-02-28 18:39:31 +0330

    testing the api on netlify

--- src/Weather.js
+++ src/Weather.js
@@ -5,6 +5,7 @@ export default function Weather() {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=bf54175800a55e59e6c4d6461deeef12&units=metric `;
   function handleResponse(response) {
     alert(response.data.main.temp);
+    console.log(response.data);
   }
   axios.get(apiUrl).then(handleResponse);
   return;
