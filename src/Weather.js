893240cf760f3dafa04afbf0efc99867a280bfcf
Matin Torkian <matin.torkian.92@gmail.com>
2023-02-28 17:37:45 +0330

    api response tested

---
+++ src/Weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather() {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=bf54175800a55e59e6c4d6461deeef12&units=metric `;
+  function handleResponse(response) {
+    alert(response.data.main.temp);
+  }
+  axios.get(apiUrl).then(handleResponse);
+  return;
+}
