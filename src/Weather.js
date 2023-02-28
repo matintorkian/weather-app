3f3a500244062ba9d117109c2c48e8d91a4c2877
Matin Torkian <matin.torkian.92@gmail.com>
2023-02-28 21:21:09 +0330

    the skeleton of the form is created

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,33 @@
 import React from "react";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Weather() {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=bf54175800a55e59e6c4d6461deeef12&units=metric `;
   function handleResponse(response) {
-    alert(response.data.main.temp);
     console.log(response.data);
   }
   axios.get(apiUrl).then(handleResponse);
-  return;
+  return (
+    <div>
+      <form className="form">
+        <input type="search" placeholder="Enter a city..." />
+        <input type="submit" value="search" />
+      </form>
+      <h1>City Name</h1>
+      <ul className="weather-description">
+        <div className="row">
+          <div className="col-6">
+            <li>Time</li>
+            <li>Description</li>
+            <li>icon</li>
+          </div>
+          <div className="col-6">
+            <li>Wind</li>
+            <li>Humidity</li>
+          </div>
+        </div>
+      </ul>
+    </div>
+  );
 }
